b0d7f8f54ab93182aab2cf3cf852599921eff3f9
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-07-21 12:24:10 +0200

    Add tests in UserSessionViewModelTests -> coverage 100%

--- EventoriasTests/ViewModelsTests/UserSessionViewModelTests.swift
+++ EventoriasTests/ViewModelsTests/UserSessionViewModelTests.swift
@@ -99,6 +99,52 @@ final class UserSessionViewModelTests: XCTestCase {
     }
     
     @MainActor
+    func testUploadImageShouldSucceed() async {
+        guard let image = loadImage(named: "charles-leclerc.jpg") else {
+            XCTFail("Failed to load image")
+            return
+        }
+        
+        let authRepositoryFake = AuthenticationRepositoryFake()
+        let userRepositoryFake = UserRepositoryFake()
+        userRepositoryFake.user = User(uid: "123", email: "charles.leclerc@ferrari.mc", fullname: "Charles Leclerc", imageURL: nil)
+        let session = UserSessionViewModel(userRepository: userRepositoryFake, authRepository: authRepositoryFake)
+        
+        await session.loadUser(by: "123")
+        await session.uploadImage(image)
+        XCTAssertNotEqual(session.currentUser?.imageURL, userRepositoryFake.user?.imageURL)
+        await session.uploadImage(image)
+    }
+    
+    @MainActor
+    func testUploadImageShouldFailedWithError() async {
+        let authRepositoryFake = AuthenticationRepositoryFake()
+        let userRepositoryFake = UserRepositoryFake()
+        userRepositoryFake.user = User(uid: "123", email: "charles.leclerc@ferrari.mc", fullname: "Charles Leclerc", imageURL: nil)
+        let session = UserSessionViewModel(userRepository: userRepositoryFake, authRepository: authRepositoryFake)
+        
+        let image = UIImage()
+        
+        await session.loadUser(by: "123")
+        await session.uploadImage(image)
+        XCTAssertEqual(session.currentUser?.imageURL, userRepositoryFake.user?.imageURL)
+        XCTAssertEqual(session.error, "Failed to upload image")
+    }
+    
+    @MainActor
+    func testUploadImageShouldFailedWithUserNotLogged() async {
+        let authRepositoryFake = AuthenticationRepositoryFake()
+        let userRepositoryFake = UserRepositoryFake()
+        let session = UserSessionViewModel(userRepository: userRepositoryFake, authRepository: authRepositoryFake)
+        
+        let image = UIImage()
+        
+        await session.uploadImage(image)
+        XCTAssertEqual(session.currentUser?.imageURL, userRepositoryFake.user?.imageURL)
+        XCTAssertEqual(session.error, "User not logged in")
+    }
+    
+    @MainActor
     func testLogout() async {
         let authRepositoryFake = AuthenticationRepositoryFake()
         let userRepositoryFake = UserRepositoryFake()
@@ -119,5 +165,15 @@ final class UserSessionViewModelTests: XCTestCase {
         
         XCTAssertNil(session.currentUser)
         XCTAssertFalse(session.isLoggedIn)
+    }
+    
+    func loadImage(named name: String) -> UIImage? {
+        let bundle = Bundle(for: type(of: self))
+        guard let url = bundle.url(forResource: name, withExtension: nil),
+              let data = try? Data(contentsOf: url),
+              let image = UIImage(data: data) else {
+            return nil
+        }
+        return image
     }
 }
