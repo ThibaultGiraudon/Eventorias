8a88a4331379c281860f4d357095a0bdebe2e45c
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-07-24 17:15:59 +0200

    Add info.plist

---
+++ Eventorias/Views/Event/EventsCalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  EventsCalendarView.swift
+//  Eventorias
+//
+//  Created by Thibault Giraudon on 24/07/2025.
+//
+
+import SwiftUI
+
+struct EventsCalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventsCalendarView()
+}
