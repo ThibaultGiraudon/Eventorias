70fee60dd3d165b5f587163906870ee5c483deb7
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-08-09 16:23:04 +0200

    Add integration test: all repository coverage 100%

---
+++ EventoriasTests/Integration/EventsRepositoryTests.swift
@@ -0,0 +1,50 @@
+//
+//  EventsRepositoryTests.swift
+//  Eventorias
+//
+//  Created by Thibault Giraudon on 09/08/2025.
+//
+
+import XCTest
+@testable import Eventorias
+
+final class EventsRepositoryTests: XCTestCase {
+
+    var eventsRepository: EventsRepository!
+    override func setUp() {
+        super.setUp()
+        
+        eventsRepository = EventsRepository()
+    }
+
+    override func tearDown() {
+        eventsRepository = nil
+        super.tearDown()
+    }
+
+    func testSetAndGetAndDeleteEventShouldSucceed() async {
+        let newEvent = Event(title: "title",
+                          descrition: "description",
+                          date: .now,
+                          hour: .now,
+                          imageURL: "",
+                          address: "ici",
+                          location: .init(latitude: 0, longitude: 0),
+                          creatorID: "123")
+        
+        do {
+            try eventsRepository.setEvent(newEvent)
+            var events = try await eventsRepository.getEvents()
+            guard let event = events.first(where: { $0.title == "title" }) else {
+                XCTFail("Failed to get event")
+                return
+            }
+            XCTAssertEqual(event.descrition, "description")
+            try await eventsRepository.clearDB()
+            events = try await eventsRepository.getEvents()
+            XCTAssertEqual(events.count, 0)
+        } catch {
+            XCTFail("Operation should not fails")
+        }
+    }
+}
