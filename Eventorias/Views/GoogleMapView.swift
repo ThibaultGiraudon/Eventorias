c1d53b50729fa4c3a7d5f7ac30045f65c0f0487b
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-08-09 13:19:54 +0200

    Update .gitignore

---
+++ Eventorias/Views/GoogleMapView.swift
@@ -0,0 +1,18 @@
+//
+//  GoogleMapView.swift
+//  Eventorias
+//
+//  Created by Thibault Giraudon on 09/08/2025.
+//
+
+import SwiftUI
+
+struct GoogleMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoogleMapView()
+}
